4448116c2ebcf966fb57c69b08c0d8e608bc81be
Duck <nguyenngocduc260504@gmail.com>
2023-11-18 17:11:03 +0700

    'update'

---
+++ BookWeb/Models/CategoryModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookWeb.Models{
+    public class Category{
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
