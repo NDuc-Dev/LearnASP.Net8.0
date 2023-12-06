49064776250dee5a2ce219e054fe664da75ecb9c
Duck <nguyenngocduc260504@gmail.com>
2023-12-06 15:38:01 +0700

    update product

--- Book.Models/Product.cs
+++ Book.Models/Product.cs
@@ -9,6 +9,7 @@ namespace Book.Models
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
+        public string Description {get; set;}
         [Required]
         public string ISBN { get; set; }
         [Required]
